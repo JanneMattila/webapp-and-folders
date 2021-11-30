eda6ed359e1d89f233cad962bde71edd78b8e9fe
Janne Mattila <janne.mattila@hotmail.fi>
2021-11-30 14:25:41 +0200

    Add support for download api

---
+++ src/WebApp/Controllers/DownloadController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+[Produces("application/json")]
+[ApiController]
+[Route("api/[controller]")]
+public class DownloadController : ControllerBase
+{
+    private readonly ILogger<DownloadController> _logger;
+
+    public DownloadController(ILogger<DownloadController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Download file.
+    /// </summary>
+    /// <param name="request">Download file request</param>
+    /// <returns>File based on the request definition</returns>
+    /// <response code="200">Returns file based on request definition</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult Post(DownloadRequest request)
+    {
+        return File(new byte[request.Size], "application/octet-stream");
+    }
+}
---
+++ src/WebApp/Models/DownloadRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models;
+
+public class DownloadRequest
+{
+    [JsonPropertyName("size")]
+    public long Size { get; set; }
+}
