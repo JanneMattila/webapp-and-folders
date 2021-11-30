8429dfe2d9aa17f89fd34406f781ad8770ff27fc
Janne Mattila <janne.mattila@hotmail.fi>
2021-11-30 14:05:46 +0200

    Add upload support

---
+++ src/WebApp/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+[Produces("application/json")]
+[ApiController]
+[Route("api/[controller]")]
+public class UploadController : ControllerBase
+{
+    private readonly ILogger<UploadController> _logger;
+
+    public UploadController(ILogger<UploadController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Upload file.
+    /// </summary>
+    /// <param name="file">Uploaded file</param>
+    /// <returns>Number of bytes uploaded</returns>
+    /// <response code="200">Returns number of bytes uploaded</response>
+    [HttpPost]
+    [ProducesResponseType(typeof(UploadResponse), StatusCodes.Status200OK)]
+    public ActionResult Post(IFormFile file)
+    {
+        return new OkObjectResult(new UploadResponse()
+        {
+            Size = file.Length
+        });
+    }
+}
---
+++ src/WebApp/Models/UploadResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models;
+
+public class UploadResponse
+{
+    [JsonPropertyName("size")]
+    public long Size { get; set; }
+}
