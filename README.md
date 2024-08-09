9c8b86f468a49f31eb49e98be53d00c1e1f6cf8b
Janne Mattila <janne.mattila@hotmail.fi>
2024-08-09 10:23:16 +0300

    Add blob streaming example

--- README.md
+++ README.md
@@ -5,3 +5,36 @@
 [![Build Status](https://dev.azure.com/jannemattila/jannemattila/_apis/build/status/JanneMattila.326-webapp-and-folders?branchName=master)](https://dev.azure.com/jannemattila/jannemattila/_build/latest?definitionId=45&branchName=master)
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Download Blob via streaming
+
+Generate large files and upload them to Blob.
+You can use Azure Cloud Shell so you don't 
+have to upload the files to the cloud.
+
+```bash
+mkdir blobtemp
+cd blobtemp
+
+head -c 500MB </dev/urandom > 500MB.bin
+head -c 1000MB </dev/urandom > 1000MB.bin
+
+sha256sum 500MB.bin > 500MB.bin.sha256
+sha256sum 1000MB.bin > 1000MB.bin.sha256
+
+cat 500MB.bin.sha256
+cat 1000MB.bin.sha256
+
+azcopy copy /home/user/blobtemp "https://<accountname>.blob.core.windows.net/demo1?<sastoken>" --recursive
+```
+
+Download the files using the API:
+
+```powershell
+curl --request GET --url 'https://webappfileblob-abc123.northeurope-01.azurewebsites.net/api/downloadblob?container=demo1&path=blobtemp/500MB.bin' --output 500MB.bin
+curl --request GET --url 'https://webappfileblob-abc123.northeurope-01.azurewebsites.net/api/downloadblob?container=demo1&path=blobtemp/500MB.bin.256' --output 500MB.bin.sha256
+
+# Check the hash
+cat 500MB.bin.sha256
+Get-FileHash -Path 500MB.bin -Algorithm SHA256
+```
---
+++ src/WebApp/Controllers/DownloadBlobController.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using Azure.Identity;
+
+namespace WebApp.Controllers;
+
+[Produces("application/json")]
+[ApiController]
+[Route("api/[controller]")]
+public class DownloadBlobController : ControllerBase
+{
+    private readonly ILogger<DownloadBlobController> _logger;
+    private readonly BlobServiceClient _blobServiceClient;
+
+    public DownloadBlobController(ILogger<DownloadBlobController> logger, IConfiguration config)
+    {
+        _logger = logger;
+
+        var blobUri = config["BlobUri"];
+        _blobServiceClient = new BlobServiceClient(new Uri(blobUri), new DefaultAzureCredential());
+    }
+
+    /// <summary>
+    /// Download blob from storage account.
+    /// </summary>
+    /// <param name="container">Container name</param>
+    /// <param name="path">Blob path</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>File based on the request definition</returns>
+    /// <response code="200">Returns file stream based on container name and path</response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> Get(string container, string path, CancellationToken cancellationToken)
+    {
+        var blobContainerClient = _blobServiceClient.GetBlobContainerClient(container);
+        var blobClient = blobContainerClient.GetBlobClient(path);
+
+        var blob = await blobClient.DownloadStreamingAsync(cancellationToken: cancellationToken);
+        return File(blob.Value.Content, blob.Value.Details.ContentType);
+    }
+}
